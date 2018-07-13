03964cdf2ab188e740ad808cc84da8fb423a5744
max <max@xyzmax.xyz>
2018-07-13 10:58:07 +0800

    添加Swagger支持

--- src/main/java/xyz/xyzmax/slam/SlamApplication.java
+++ src/main/java/xyz/xyzmax/slam/SlamApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import io.swagger.annotations.ApiOperation;
 
 @SpringBootApplication
 public class SlamApplication {
@@ -15,9 +18,10 @@ public class SlamApplication {
 	@RestController
 	public class TestController {
 
-		@RequestMapping("/test")
-		public String Test() {
-			return this.toString();
+		@ApiOperation(value = "测试接口", notes = "测试接口详细描述")
+		@RequestMapping(value = "/test", method = RequestMethod.GET)
+		public String get() {
+			return "get";
 		}
 	}
 }
